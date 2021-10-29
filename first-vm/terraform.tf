293f67b68d0c2d9f054d60ee39d97accf0c82133
toleksa <toleksa@gmail.com>
2021-10-30 00:34:07 +0200

    commit

---
+++ first-vm/terraform.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_providers {
+    libvirt = {
+      source = "dmacvicar/libvirt"
+      version = "0.6.11"
+    }
+  }
+}
+
+provider "libvirt" {
+  uri   = "qemu+ssh://root@192.168.0.3/system"
+}
+
+#resource "libvirt_pool" "default" {
+#  name = "default"
+#  type = "dir"
+#  path = "/virtualki/default/"
+#}
+
+resource "libvirt_volume" "centos7-qcow2" {
+  name = "centos7.qcow2"
+  pool = "default"
+  source = "https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2"
+  format = "qcow2"
+}
+
+# Define KVM domain to create
+resource "libvirt_domain" "first" {
+  name   = "first"
+  memory = "1024"
+  vcpu   = 1
+
+  network_interface {
+    network_name = "default"
+  }
+
+  disk {
+    volume_id = "${libvirt_volume.centos7-qcow2.id}"
+  }
+
+  console {
+    type = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+
+  graphics {
+    type = "spice"
+    listen_type = "address"
+    autoport = true
+  }
+}
+
+#output "ip" {
+#  value = "${libvirt_domain.first.network_interface.0.addresses.0}"
+#}
+
--- install-terraform.sh
+++ install-terraform.sh
@@ -2,7 +2,19 @@
 
 set -euo pipefail
 
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-apt install terraform
+# ubuntu
+#curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+#apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+#apt install terraform
+
+# fedora
+dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
+dnf install terraform
+
+#mkdir -p ~/.terraform.d/plugins/
+#wget https://github.com/dmacvicar/terraform-provider-libvirt/releases/download/v0.6.11/terraform-provider-libvirt_0.6.11_linux_amd64.zip -O /tmp/terraform-libvirt.zip
+#tar xvf terraform-provider-libvirt-0.6.0+git.1569597268.1c8597df.Fedora_28.x86_64.tar.gz
+#mv terraform-provider-libvirt ~/.terraform.d/plugins/
+
+ssh-add ~/.ssh/id_rsa 
 
