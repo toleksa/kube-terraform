4e54b964e24a515818cbc0f1847a0ddedbc5f7d2
toleksa <toleksa@gmail.com>
2021-10-30 01:38:57 +0200

    cloudinit

--- first-vm/terraform.tf
+++ first-vm/terraform.tf
@@ -28,7 +28,18 @@ resource "libvirt_volume" "centos7-qcow2" {
   name = "centos7.qcow2"
   pool = "default"
   source = "https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2"
+  #source = "/virtualki/templates/CentOS-7-x86_64-GenericCloud.qcow2"
   format = "qcow2"
+}
+
+data "template_file" "user_data" {
+  template = "${file("${path.module}/cloud_init.cfg")}"
+}
+
+# Use CloudInit to add the instance
+resource "libvirt_cloudinit_disk" "commoninit" {
+  name = "commoninit.iso"
+  user_data      = "${data.template_file.user_data.rendered}"
 }
 
 # Define KVM domain to create
@@ -38,12 +49,17 @@ resource "libvirt_domain" "first" {
   vcpu   = 1
 
   network_interface {
-    network_name = "${libvirt_network.br0.name}"
+#    network_name = "${libvirt_network.br0.name}"
+    network_name = "default"
+    hostname = "first"
+    wait_for_lease = true
   }
 
   disk {
     volume_id = "${libvirt_volume.centos7-qcow2.id}"
   }
+
+  cloudinit = "${libvirt_cloudinit_disk.commoninit.id}"
 
   console {
     type = "pty"
@@ -58,7 +74,7 @@ resource "libvirt_domain" "first" {
   }
 }
 
-#output "ip" {
-#  value = "${libvirt_domain.first.network_interface.0.addresses.0}"
-#}
+output "ip" {
+  value = "${libvirt_domain.first.network_interface.0.addresses.0}"
+}
 
