62e41eb3bff64e3589232f08bd06c2152f52fd71
toleksa <toleksa@gmail.com>
2021-11-01 13:48:49 +0100

    added sudo

--- install-terraform.sh
+++ install-terraform.sh
@@ -5,14 +5,14 @@ set -euo pipefail
 # ubuntu
 if [ -x /usr/bin/apt ]; then
   curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-  apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-  apt install terraform
+  sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+  sudo apt install terraform
 fi
 
 # fedora
 if [ -x /usr/bin/dnf ]; then
-  dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
-  dnf install terraform
+  sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
+  sudo dnf install terraform
 fi
 
 ssh-add ~/.ssh/id_rsa 
