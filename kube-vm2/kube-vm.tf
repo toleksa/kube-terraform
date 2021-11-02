baef5721d6665ea1bf946ae38cf8ce02ec703c2e
toleksa <toleksa@gmail.com>
2021-11-02 16:16:38 +0100

    multiple VM deployment

---
+++ kube-vm2/kube-vm.tf
@@ -0,0 +1,100 @@
+# vim: syntax=yaml
+variable "host_count" {
+  default = "4"
+}
+
+terraform {
+  required_providers {
+    libvirt = {
+      source = "dmacvicar/libvirt"
+      version = "0.6.11"
+    }
+  }
+}
+
+provider "libvirt" {
+  uri   = "qemu+ssh://root@192.168.0.3/system"
+}
+
+resource "libvirt_pool" "kube" {
+  name = "kube"
+  type = "dir"
+  path = "/virtualki/kube"
+}
+
+resource "libvirt_network" "kube" {
+  name = "kube"
+  mode = "bridge"
+  bridge = "br0"
+  autostart = "true"
+}
+
+resource "libvirt_volume" "volume" {
+  count = "${var.host_count}"
+  name = "v${count.index + 1}"
+  pool = "kube"
+  #source = "https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64-disk-kvm.img"
+  source = "http://192.168.0.3/focal-server-cloudimg-amd64-disk-kvm.img"
+  format = "qcow2"
+}
+
+data "template_file" "meta_data" {
+  count = "${var.host_count}"
+  template = file("${path.module}/meta_data.cfg")
+  vars = {
+    HOSTNAME = "v${count.index + 1}.kube.ac"
+    FQDN = "v${count.index + 1}.kube.ac" 
+    ID = "ID-v${count.index + 1}"
+  }
+}
+
+data "template_file" "user_data" {
+  template = file("${path.module}/user_data.cfg")
+}
+
+# Use CloudInit to add the instance
+resource "libvirt_cloudinit_disk" "cloudinit" {
+  count = "${var.host_count}"
+  name = "cloudinit${count.index + 1}.iso"
+  meta_data  = "${data.template_file.meta_data[count.index].rendered}"
+  user_data  = "${data.template_file.user_data.rendered}"
+  pool = "kube"
+}
+
+# Define KVM domain to create
+resource "libvirt_domain" "virtkubes" {
+  count = "${var.host_count}"
+  name   = "v${count.index + 1}"
+  memory = "3500"
+  vcpu   = 3
+
+  network_interface {
+    network_name = "${libvirt_network.kube.name}"
+    #network_name = "default"
+    #wait_for_lease = true
+    hostname = "v${count.index + 1}.kube.ac"
+  }
+
+  disk {
+    volume_id = "${libvirt_volume.volume[count.index].id}"
+  }
+
+  cloudinit = "${libvirt_cloudinit_disk.cloudinit[count.index].id}"
+
+  console {
+    type = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+
+  graphics {
+    type = "spice"
+    listen_type = "address"
+    autoport = true
+  }
+}
+
+#output "ip" {
+#  value = "${libvirt_domain.virtkube1.network_interface.0.addresses.0}"
+#}
+
