3280ff988e3ca6ad0c15d5144734cded7d43195d
toleksa <toleksa@gmail.com>
2021-12-04 01:05:28 +0200

    disable postgres pool and network as this is fuckin' annoying

--- postgres/postgres.tf
+++ postgres/postgres.tf
@@ -16,23 +16,24 @@ provider "libvirt" {
   uri   = "qemu+ssh://root@192.168.0.3/system"
 }
 
-resource "libvirt_pool" "postgres" {
-  name = "postgres"
-  type = "dir"
-  path = "/virtualki/postgres"
-}
+#resource "libvirt_pool" "postgres" {
+#  name = "postgres"
+#  type = "dir"
+#  path = "/virtualki/postgres"
+#}
 
-resource "libvirt_network" "postgres" {
-  name = "postgres"
-  mode = "bridge"
-  bridge = "br0"
-  autostart = "true"
-}
+#resource "libvirt_network" "postgres" {
+#  name = "postgres"
+#  mode = "bridge"
+#  bridge = "br0"
+#  autostart = "true"
+#}
 
 resource "libvirt_volume" "volume" {
   count = "${var.host_count}"
   name = "postgres${count.index + 1}"
-  pool = "postgres"
+  #pool = "postgres"
+  pool = "default"
   #source = "https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64-disk-kvm.img"
   #source = "http://192.168.0.3/focal-server-cloudimg-amd64-disk-kvm.img"
   source = "http://cloud.centos.org/centos/8/x86_64/images/CentOS-8-GenericCloud-8.4.2105-20210603.0.x86_64.qcow2"
@@ -59,7 +60,8 @@ resource "libvirt_cloudinit_disk" "cloudinit" {
   name = "cloudinit${count.index + 1}.iso"
   meta_data  = "${data.template_file.meta_data[count.index].rendered}"
   user_data  = "${data.template_file.user_data.rendered}"
-  pool = "postgres"
+  #pool = "postgres"
+  pool = "default"
 }
 
 # Define KVM domain to create
@@ -70,8 +72,8 @@ resource "libvirt_domain" "virtkubes" {
   vcpu   = 2
 
   network_interface {
-    network_name = "${libvirt_network.postgres.name}"
-    #network_name = "default"
+    #network_name = "${libvirt_network.postgres.name}"
+    network_name = "default"
     #wait_for_lease = true
     hostname = "postgres${count.index + 1}"
   }
