3f6c2f91557923fc1a403efc3fef502af1f62f39
toleksa <toleksa@gmail.com>
2021-12-20 16:35:02 +0100

    update

--- postgres/run.sh
+++ postgres/run.sh
@@ -2,8 +2,8 @@
 
 terraform apply -auto-approve
 
-sleep 60s
 
+export ANSIBLE_CONFIG=./ansible/ansible.cfg
 ansible-playbook -i ansible/inv ansible/install-postgres.yaml
 ansible-playbook -i ansible/inv ansible/data-postgres.yaml
 ansible-playbook -i ansible/inv ansible/replication-postgres.yaml
