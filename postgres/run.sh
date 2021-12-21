5f2b7ca6c1847560a3a39b74b2cf08825d0f9107
toleksa <toleksa@gmail.com>
2021-12-21 14:36:27 +0100

    failover and recovery

--- postgres/run.sh
+++ postgres/run.sh
@@ -8,3 +8,4 @@ ansible-playbook -i ansible/inv ansible/install-postgres.yaml
 ansible-playbook -i ansible/inv ansible/data-postgres.yaml
 ansible-playbook -i ansible/inv ansible/replication-postgres.yaml
 
+echo "ANSIBLE_CONFIG=./ansible/ansible.cfg; ansible-playbook -i ansible/inv ansible/failover-and-recovery.yaml"
