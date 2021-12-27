44e901d8b2ff9643c9461a7837f5097f295764e3
toleksa <toleksa@gmail.com>
2021-12-27 14:56:03 +0100

    info about upgrade

--- postgres/run.sh
+++ postgres/run.sh
@@ -9,3 +9,5 @@ ansible-playbook -i ansible/inv ansible/data-postgres.yaml
 ansible-playbook -i ansible/inv ansible/replication-postgres.yaml
 
 echo "ANSIBLE_CONFIG=./ansible/ansible.cfg; ansible-playbook -i ansible/inv ansible/failover-and-recovery.yaml"
+echo "OR"
+echo "ANSIBLE_CONFIG=./ansible/ansible.cfg; ansible-playbook -i ansible/inv ansible/upgrade-postgres.yaml"
