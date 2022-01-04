7a62083e5ac3098740c13b4926c39e9a3c7f945c
toleksa <toleksa@gmail.com>
2022-01-04 13:40:48 +0100

    typo

--- postgres/run.sh
+++ postgres/run.sh
@@ -8,6 +8,6 @@ ansible-playbook -i ansible/inv ansible/install-postgres.yaml
 ansible-playbook -i ansible/inv ansible/data-postgres.yaml
 ansible-playbook -i ansible/inv ansible/replication-postgres.yaml
 
-echo "ANSIBLE_CONFIG=./ansible/ansible.cfg; ansible-playbook -i ansible/inv ansible/failover-and-recovery.yaml"
+echo "ANSIBLE_CONFIG=./ansible/ansible.cfg ansible-playbook -i ansible/inv ansible/failover-and-recovery.yaml"
 echo "OR"
-echo "ANSIBLE_CONFIG=./ansible/ansible.cfg; ansible-playbook -i ansible/inv ansible/upgrade-postgres.yaml"
+echo "ANSIBLE_CONFIG=./ansible/ansible.cfg ansible-playbook -i ansible/inv ansible/upgrade-postgres.yaml"
