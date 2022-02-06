d21fbfb30c70cfcabf7a55a1423826a987884608
toleksa <toleksa@gmail.com>
2022-02-06 13:30:46 +0200

    use better repo

--- postgres/postgres.tf
+++ postgres/postgres.tf
@@ -35,8 +35,7 @@ resource "libvirt_volume" "volume" {
   #pool = "postgres"
   pool = "default"
 #  source = "http://cloud.centos.org/centos/8/x86_64/images/CentOS-8-GenericCloud-8.4.2105-20210603.0.x86_64.qcow2"
-  source = "http://almalinux-mirror.datakeepers.co.za/8.5/cloud/x86_64/images/AlmaLinux-8-GenericCloud-8.5-20211119.x86_64.qcow2"
-#  source = "http://almalinux-mirror.datakeepers.co.za/8.5/cloud/x86_64/images/AlmaLinux-8-GenericCloud-latest.x86_64.qcow2"
+  source = "http://ftp.linux.cz/pub/linux/almalinux/8.5/cloud/x86_64/images/AlmaLinux-8-GenericCloud-latest.x86_64.qcow2"
   format = "qcow2"
 }
 
