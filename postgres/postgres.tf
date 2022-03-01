e45598a2c1a8343079d85383079bd78af22e3776
toleksa <toleksa@gmail.com>
2022-03-01 13:27:09 +0100

    use local repo

--- postgres/postgres.tf
+++ postgres/postgres.tf
@@ -35,7 +35,8 @@ resource "libvirt_volume" "volume" {
   #pool = "postgres"
   pool = "default"
 #  source = "http://cloud.centos.org/centos/8/x86_64/images/CentOS-8-GenericCloud-8.4.2105-20210603.0.x86_64.qcow2"
-  source = "http://ftp.linux.cz/pub/linux/almalinux/8.5/cloud/x86_64/images/AlmaLinux-8-GenericCloud-latest.x86_64.qcow2"
+#  source = "http://ftp.linux.cz/pub/linux/almalinux/8.5/cloud/x86_64/images/AlmaLinux-8-GenericCloud-latest.x86_64.qcow2"
+  source = "http://192.168.0.2:8765/AlmaLinux-8-GenericCloud-latest.x86_64.qcow2"
   format = "qcow2"
 }
 
