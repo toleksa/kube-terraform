c669e1eeb22c995d89e3dfe4e2958cc823a1acd9
toleksa <toleksa@gmail.com>
2022-03-09 09:42:16 +0200

    image fix

--- first-vm/first-vm.tf
+++ first-vm/first-vm.tf
@@ -30,14 +30,13 @@ resource "libvirt_volume" "first-ubuntu-20_04" {
   pool = "${libvirt_pool.first-vm.name}"
   #source = "https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2"
   #source = "/virtualki/templates/CentOS-7-x86_64-GenericCloud.qcow2"
-  source = "https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64-disk-kvm-50G.img"
+  source = "https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64-disk-kvm.img"
   format = "qcow2"
 }
 
 data "template_file" "user_data" {
   template = "${file("${path.module}/user_data.cfg")}"
 }
-
 
 # Use CloudInit to add the instance
 resource "libvirt_cloudinit_disk" "commoninit" {
