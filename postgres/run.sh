5c09087aff8350e2de96af34809da7fdabc3400e
toleksa <toleksa@gmail.com>
2022-03-09 11:03:07 +0200

    update reverse replication script

--- postgres/run.sh
+++ postgres/run.sh
@@ -8,6 +8,6 @@ ansible-playbook -i ansible/inv ansible/install-postgres.yaml
 ansible-playbook -i ansible/inv ansible/data-postgres.yaml
 ansible-playbook -i ansible/inv ansible/replication-postgres.yaml
 
-echo "ANSIBLE_CONFIG=./ansible/ansible.cfg ansible-playbook -i ansible/inv ansible/reverse-replication.yaml"
+echo "ANSIBLE_CONFIG=./ansible/ansible.cfg ansible-playbook -i ansible/inv ansible/reverse-replication-11.yaml"
 echo "OR"
 echo "ANSIBLE_CONFIG=./ansible/ansible.cfg ansible-playbook -i ansible/inv ansible/upgrade-postgres.yaml"
