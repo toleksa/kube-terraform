8769cd2772062d234cfb48bb2cc5689a5d2f015e
toleksa <toleksa@gmail.com>
2022-04-03 11:23:35 +0200

    formatting

--- postgres/reset.sh
+++ postgres/reset.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# revert to initial postgres 11 replication state with clean DB
+
 ansible-playbook -i ansible/inv ansible/stop-and-reset.yaml
 ansible-playbook -i ansible/inv ansible/install-postgres.yaml --skip-tags='repo'
 ansible-playbook -i ansible/inv ansible/data-postgres.yaml
