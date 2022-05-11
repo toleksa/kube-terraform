e8901b1444780d432e1a6f2aa5512c9a1d0b8dd2
toleksa <toleksa@gmail.com>
2022-05-11 12:42:06 +0200

    working version

---
+++ kube-cluster/kube-server.tf
@@ -0,0 +1,67 @@
+# vim: syntax=yaml
+resource "libvirt_volume" "kube-server" {
+  count           = var.server_count
+  name            = "kube-${var.cluster_name}-server-${count.index}-disk"
+  base_volume_id  = libvirt_volume.kube.id
+  pool            = libvirt_volume.kube.pool
+  format          = "qcow2"
+}
+
+resource "libvirt_cloudinit_disk" "server-init" {
+  count          = var.server_count
+  name           = "kube-${var.cluster_name}-server-${count.index}-init.iso"
+  user_data      = data.template_file.server_user_data[count.index].rendered
+  pool           = libvirt_volume.kube.pool
+}
+
+data "template_file" "server_user_data" {
+  count = "${var.server_count}"
+  template = format("%s%s",file("${path.module}/../user_data_base.cfg"),file("${path.module}/user_data_kube.cfg"))
+  vars = {
+    HOSTNAME = "s${count.index + 1}.${var.cluster_name}.kube.ac",
+    JOIN_ADDR = "s1.${var.cluster_name}.kube.ac"
+    JOIN_TOKEN = "${var.join_token}"
+  }
+}
+
+# Define KVM domain to create
+resource "libvirt_domain" "kube" {
+  count = "${var.server_count}"
+  name   = "v${count.index + 1}"
+  memory = "${var.server_mem}"
+  vcpu   = "${var.server_vcpu}"
+
+  network_interface {
+    network_id = "${libvirt_network.kube.id}"
+    wait_for_lease = true
+    mac = "44:8a:5b:00:03:0${count.index + 1}"
+    hostname = "s${count.index + 1}.${var.cluster_name}.kube.ac"
+  }
+
+  disk {
+    volume_id = "${libvirt_volume.kube-server[count.index].id}"
+  }
+
+  cloudinit = "${libvirt_cloudinit_disk.server-init[count.index].id}"
+
+  console {
+    type = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+
+  graphics {
+    type = "spice"
+    listen_type = "address"
+    autoport = true
+  }
+  qemu_agent = true
+}
+
+output "ip" {
+  value = {
+    for host in libvirt_domain.kube:
+    host.name => host.network_interface.0.addresses.*
+  }
+}
+
