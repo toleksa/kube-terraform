fdcf7e6a40bdf896e96dadba28f938f953812918
toleksa <toleksa@gmail.com>
2022-05-18 13:52:27 +0200

    run for multiple environments

--- kube-cluster/run.sh
+++ kube-cluster/run.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
-terraform apply -var-file=variables.tfvars --auto-approve
+if [ $# -lt 1 ]; then
+  echo "usage: $0 <env> [apply|destroy]"
+  exit 0
+fi
+
+ENV_DIR="$1"
+
+if [ ! -d "env/$ENV_DIR" ]; then
+  echo "ERR: env/$ENV_DIR dir not found"
+  exit 1
+fi
+
+if [ ! -d "env/$ENV_DIR/.terraform" ]; then
+  echo "INFO: env/$ENV_DIR/.terraform not found, initializing terraform"
+  TF_DATA_DIR=env/$ENV_DIR/.terraform terraform init --backend-config="path=env/$ENV_DIR/terraform.tfstate"
+  echo "INFO: init finished"
+fi
+
+CMD="TF_DATA_DIR=env/$ENV_DIR/.terraform terraform ${2:-apply} -var-file=env/$ENV_DIR/variables.tfvars" #--backend-config=\"path=env/$ENV_DIR/terraform.tfstate\" -var-file=env/$ENV_DIR/variables.tfvars"
+echo "EXEC: $CMD"
+eval $CMD
 
