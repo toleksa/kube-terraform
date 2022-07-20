18e9147435ce1f2ea006ab79086c2e9a29add83e
toleksa <toleksa@gmail.com>
2022-07-20 19:08:12 +0200

    select cluster as parameter

--- ansible-terraform/004/run.sh
+++ ansible-terraform/004/run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-ansible-playbook -i inventory deploy.yaml --extra-vars "variable_host=virtbox"
+if [ $# -ne 1 ]; then
+  echo "ERR: usage: $0 <cluster>"
+  exit 1
+fi
+
+ansible-playbook -i inventory deploy.yaml --extra-vars "variable_host=$1"
 
