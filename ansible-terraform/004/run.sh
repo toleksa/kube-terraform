5236002d97774b677c2e18a2c309fc450450f8b3
toleksa <toleksa@gmail.com>
2022-07-27 08:09:50 +0200

    separate inventories

--- ansible-terraform/004/run.sh
+++ ansible-terraform/004/run.sh
@@ -5,7 +5,7 @@ if [ $# -lt 1 ]; then
   exit 1
 fi
 
-if [ ! -d "inventory/group_vars/$1" ]; then
+if [ ! -d "inventory/$1/group_vars/$1" ]; then
   echo "ERR: group_vars for $1 not found"
   exit 4
 fi
@@ -28,7 +28,7 @@ else
   exit 2
 fi
 
-CMD="ansible-playbook -i inventory deploy.yaml --extra-vars \"variable_host=$1\" --extra-vars \"terraform_action=$terraform_action\""
+CMD="ansible-playbook -i inventory/$1 deploy.yaml --extra-vars \"variable_host=$1\" --extra-vars \"terraform_action=$terraform_action\""
 echo $CMD
 eval $CMD
 
