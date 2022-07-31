7001c8853b301ed60559002c41d1b13a1a079f5d
toleksa <toleksa@gmail.com>
2022-07-31 14:06:11 +0200

    init - beta version

---
+++ ansible-terraform/005/run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+  echo "ERR: usage: $0 <cluster> [apply|destroy]"
+  exit 1
+fi
+
+if [ ! -d "inventory/$1/group_vars/$1" ]; then
+  echo "ERR: group_vars for $1 not found"
+  exit 4
+fi
+
+export IAC_PROFILE="$1"
+
+if [ ! -f .env ]; then
+  echo "ERR: .env file missing"
+  exit 3
+fi
+
+. .env
+
+if [ "$2" == "destroy" ]; then
+  terraform_action="destroy"
+elif [ "$2" == "apply" ] || [ $# -eq 1 ]; then
+  terraform_action="apply"
+else
+  echo "unrecognized command parameter"
+  exit 2
+fi
+
+CMD="ansible-playbook -i inventory/$1 deploy.yaml --extra-vars \"variable_host=$1\" --extra-vars \"terraform_action=$terraform_action\""
+#CMD="ansible-playbook -i inventory/$1 deploy.yaml --extra-vars \"variable_host=$1\" --extra-vars \"terraform_action=$terraform_action\" --start-at-task=\"helm install argocd\""
+echo $CMD
+eval $CMD
+
