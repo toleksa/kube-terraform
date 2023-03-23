7ab27fa49c3849b742e3bff39b1752bcd107d652
toleksa <toleksa@gmail.com>
2023-03-24 01:50:22 +0200

    variables.tf - cleanup

--- oci/001/variables.tf
+++ oci/001/variables.tf
@@ -1,6 +1,3 @@
-## Copyright © 2020, Oracle and/or its affiliates. 
-## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
-
 variable "tenancy_ocid" {}
 variable "compartment_ocid" {}
 variable "region" {}
@@ -9,23 +6,5 @@ variable "private_key_path" {}
 variable "user_ocid" {}
 variable "availablity_domain_name" {
   default = ""
-}
-
-variable "node_shape" {
-  default = "VM.Standard.E2.1.Micro"
-}
-
-variable "node_flex_shape_memory" {
-  default = 4
-}
-
-variable "instance_os" {
-  description = "Operating system for compute instances"
-  default     = "Oracle Linux"
-}
-
-variable "linux_os_version" {
-  description = "Operating system version for all Linux instances"
-  default     = "8"
 }
 
