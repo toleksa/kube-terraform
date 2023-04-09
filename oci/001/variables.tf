abf431a710ba116c526f9a1f41eb032401f52a2f
toleksa <toleksa@gmail.com>
2023-04-09 15:09:42 +0200

    instance_shape and instance_source default values

--- oci/001/variables.tf
+++ oci/001/variables.tf
@@ -8,5 +8,10 @@ variable "user_ocid" {}
 variable "availablity_domain_name" {
   default = ""
 }
-variable "instance_shape" {}
-variable "instance_source" {}
+variable "instance_shape" {
+  default = "VM.Standard.E2.1.Micro"
+}
+variable "instance_source" {
+  #Oracle-Linux-8.7-2023.05.24-0
+  default = "ocid1.image.oc1.eu-zurich-1.aaaaaaaad5t3txkxdpokv5ejxodnwl4nx3xyyme3bvl2u5sauqjdyv4dtpba"
+}
