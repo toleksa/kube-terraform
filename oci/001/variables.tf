33c2a15cfdf440c3fbb9f11bf946e2d3b448af2b
toleksa <toleksa@gmail.com>
2023-04-13 00:04:03 +0200

    variables.tf - formatting

--- oci/001/variables.tf
+++ oci/001/variables.tf
@@ -12,6 +12,5 @@ variable "instance_shape" {
   default = "VM.Standard.E2.1.Micro"
 }
 variable "instance_source" {
-  #Oracle-Linux-8.7-2023.05.24-0
-  default = "ocid1.image.oc1.eu-zurich-1.aaaaaaaad5t3txkxdpokv5ejxodnwl4nx3xyyme3bvl2u5sauqjdyv4dtpba"
+  default = "ocid1.image.oc1.eu-zurich-1.aaaaaaaad5t3txkxdpokv5ejxodnwl4nx3xyyme3bvl2u5sauqjdyv4dtpba"    #Oracle-Linux-8.7-2023.05.24-0
 }
