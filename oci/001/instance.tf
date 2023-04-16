95810b12b3be7210a9396ab345fd814b7d732276
toleksa <toleksa@gmail.com>
2023-04-16 21:32:39 +0200

    instance.tf - add network components

--- oci/001/instance.tf
+++ oci/001/instance.tf
@@ -1,3 +1,50 @@
+resource "oci_core_vcn" "vcn1" {
+  cidr_block     = "10.0.0.0/16"
+  compartment_id = var.compartment_ocid
+  display_name   = "vcn1"
+  dns_label      = "vcn1"
+}
+
+resource "oci_core_subnet" "subnet1" {
+  availability_domain = var.availablity_domain_name
+  cidr_block          = "10.0.0.0/24"
+  display_name        = "subnet1"
+  compartment_id      = var.compartment_ocid
+  vcn_id              = oci_core_vcn.vcn1.id
+  route_table_id      = oci_core_vcn.vcn1.default_route_table_id
+  security_list_ids   = [oci_core_vcn.vcn1.default_security_list_id,oci_core_security_list.security_list1.id]
+  dhcp_options_id     = oci_core_vcn.vcn1.default_dhcp_options_id
+  dns_label           = "subnet1"
+}
+
+resource "oci_core_security_list" "security_list1" {
+    #Required
+    compartment_id = var.compartment_ocid
+    vcn_id = oci_core_vcn.vcn1.id
+
+    #Optional
+    display_name = "security_list1"
+    ingress_security_rules {
+        #Required
+        protocol = 6  #TCP
+        source = "0.0.0.0/0"
+
+        #Optional
+        description = "http"
+        source_type = "CIDR_BLOCK"
+        stateless = false
+        tcp_options {
+            #Optional
+            max = 80
+            min = 80
+        }
+    }
+    egress_security_rules {
+        protocol = 6  #TCP
+        destination = "0.0.0.0/0"
+    }
+}
+
 resource "oci_core_instance" "vm1" {
     availability_domain = var.availablity_domain_name
     compartment_id = var.compartment_ocid
