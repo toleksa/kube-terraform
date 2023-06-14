cdb2c04525ef0b7750afc3ff4b285e72b0a4f783
toleksa <toleksa@gmail.com>
2023-06-14 13:12:54 +0200

    oci/004 - run counter

--- oci/004-k3s/run.sh
+++ oci/004-k3s/run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+COUNTER=0
 while true; do
   terraform apply --auto-approve
   if [ $? -eq 0 ]; then
     /root/alert.sh "TA OCI is done"
     exit 0
   fi
-  echo -e .
+  COUNTER=$((COUNTER+1))
+  echo "counter: $COUNTER"
   sleep 10s
 done
