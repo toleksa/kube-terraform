6548f8f2d29f220225d6af36a0301fa338eb419f
toleksa <toleksa@gmail.com>
2023-11-06 14:53:56 +0200

    test.sh - wait for all argocd apps to be healthy

--- ansible-terraform/006/test.sh
+++ ansible-terraform/006/test.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 #terraform apply --auto-approve
-./run.sh virtbox apply --auto-approve
+./run.sh ivy2 apply --auto-approve
 
-URL="argocd.c0.kube.ac"
+URL="argocd.c2.kube.ac"
 until curl --output /dev/null --silent --head --fail "$URL" ; do
     sleep 1s
     echo -n .
-done
+done ; echo OK
+
+ssh-keygen -R s0.c2.kube.ac
+ssh -o StrictHostKeyChecking=accept-new root@s0.c2.kube.ac "while argocd app list | grep -v HEALTH | grep -v Healthy > /dev/null ; do sleep 1s ; echo -n . ; done ; argocd app list"
 
