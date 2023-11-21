7292c5a9b2bea248479e884c61c0d5ab1f7f690e
toleksa <toleksa@gmail.com>
2023-11-21 06:31:40 +0200

    hyperv - .env for argocd

--- ansible-terraform/hyper-v/run.sh
+++ ansible-terraform/hyper-v/run.sh
@@ -5,6 +5,13 @@ if [ $# -eq 0 ]; then
   exit 0
 fi
 
+if [ ! -f .env ]; then
+  echo "ERR: .env file missing"
+  exit 3
+fi
+
+. .env
+
 if [ "$1" == "stop" ]; then
   terraform destroy -auto-approve
   exit 0
