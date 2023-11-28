7819067e7da997aecb0bd55cdf551ab9761bc0bf
toleksa <toleksa@gmail.com>
2023-11-28 18:12:09 +0200

    hyperv - merge provider.tf to main.tf

--- ansible-terraform/hyper-v/main.tf
+++ ansible-terraform/hyper-v/main.tf
@@ -9,6 +9,23 @@ terraform {
   }
 }
 
+provider "hyperv" {
+  user            = var.HYPERV_USER
+  password        = var.HYPERV_PASS
+  host            = var.HYPERV_HOST
+  port            = 5985
+  https           = false
+  insecure        = true
+  use_ntlm        = true
+  tls_server_name = ""
+  cacert_path     = ""
+  cert_path       = ""
+  key_path        = ""
+  #script_path     = "C:/Temp/terraform_%RAND%.cmd"
+  timeout         = "30s"
+}
+
+
 #resource "hyperv_network_switch" "dmz_network_switch" {
 #  name = "dmz"
 #}
@@ -77,4 +94,8 @@ resource "hyperv_machine_instance" "kube1" {
     enable_secure_boot = "Off"
   }
 }
+
+variable "HYPERV_USER" {}
+variable "HYPERV_PASS" {}
+variable "HYPERV_HOST" {}
 
